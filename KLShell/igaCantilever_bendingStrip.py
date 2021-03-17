a6b7b0b6d271635d6acf0f843423afdbb511ae13
felgueta1995 <felgueta@miuandes.cl>
2021-03-17 00:21:45 -0300

    Added functional bending strip, ugly solution, need to work on smart visualization and surface handling

--- KLShell/igaCantilever_bendingStrip.py
+++ KLShell/igaCantilever_bendingStrip.py
@@ -59,13 +59,13 @@ surfR = operations.translate(surfL, [La, 0, 0])
 
 
 # Creating bending strip
-controlPtsL = surfL.ctrlpts2d[:] # This gives v,u format
-controlPtsL = np.array(compatibility.flip_ctrlpts2d(controlPtsL)) # This flips it to u,v
-interfacePoints= controlPtsL[-1]
+controlPtsL = surfL.ctrlpts2d[:]  # This gives v,u format
+controlPtsL = np.array(compatibility.flip_ctrlpts2d(controlPtsL))  # This flips it to u,v
+interfacePoints = controlPtsL[-1]
 nodesOnLeftPatch = controlPtsL[-2]
 
-controlPtsR = surfR.ctrlpts2d[:] # This gives v,u format
-controlPtsR = np.array(compatibility.flip_ctrlpts2d(controlPtsR)) # This flips it to u,v
+controlPtsR = surfR.ctrlpts2d[:]  # This gives v,u format
+controlPtsR = np.array(compatibility.flip_ctrlpts2d(controlPtsR))  # This flips it to u,v
 nodesOnRightPatch = controlPtsR[1]
 
 print("nodesOnLeftPatch: ", nodesOnLeftPatch)
@@ -73,7 +73,7 @@ print("interfacePoints: ", interfacePoints)
 print("nodesOnRightPatch: ", nodesOnRightPatch)
 
 # These are in u,v
-controlPtsBendingStrip=np.concatenate([nodesOnLeftPatch,interfacePoints,nodesOnRightPatch])
+controlPtsBendingStrip = np.concatenate([nodesOnLeftPatch, interfacePoints, nodesOnRightPatch])
 print("controlPtsBendingStrip: ", controlPtsBendingStrip)
 
 bendingStrip = NURBS.Surface()
@@ -133,7 +133,8 @@ ops.nDMaterial("ElasticOrthotropicPlaneStress", tagPlaneStress2, E1, E2, nu12, n
 
 tagPlaneStress3 = 3
 # ops.nDMaterial("PlaneStress", tagPlaneStress2, tagNDmat2)
-ops.nDMaterial("ElasticOrthotropicPlaneStress", tagPlaneStress3, E1, E2, nu12, nu21, rho)
+ops.nDMaterial("ElasticOrthotropicPlaneStress", tagPlaneStress3, E1 * 30000000, E2 * 30000000, 0, 0, rho)
+# ops.nDMaterial("ElasticOrthotropicPlaneStress", tagPlaneStress3, 0, 0, nu12, nu21, rho)
 
 
 deg2rad = pi / 180
@@ -151,28 +152,28 @@ gFact = [0.0, 0.0, 0.0 * 9.807]
 
 Nlayers = len(θ)
 
-names=["leftPatch","rightPatch","bendingStrip"]
+names = ["leftPatch", "rightPatch", "bendingStrip"]
+shellType = 'KLShell'
 
 for i in range(len(container)):
-    surf=container[i]
-    name=names[i]
-    shellType='KLShell'
-    if name=="bendingStrip":
-        shellType="KLShell_BendingStrip"
+    surf = container[i]
+    name = names[i]
+    if name == "bendingStrip":
+        shellType = "KLShell_BendingStrip"
 
     # Flipping control point to u,v format
     controlPts = surf.ctrlpts2d[:]
     controlPts = np.array(compatibility.flip_ctrlpts2d(controlPts))
 
     # Creating a Patch in OpenSees
-    ops.IGA("Patch", patchTag, nodeStartTag, P, Q, noPtsX, noPtsY,
+    ops.IGA("Patch", patchTag, nodeStartTag, surf.degree_u, surf.degree_v, surf.ctrlpts_size_u, surf.ctrlpts_size_v,
             "-type", shellType,
             "-nonLinearGeometry", 0,
             "-planeStressMatTags", *matTags,
             "-gFact", *gFact,
             "-theta", *θ,
             "-thickness", *thickness,
-            "-uKnot", *uKnot, "-vKnot", *vKnot, "-controlPts", *controlPts.flatten())
+            "-uKnot", *surf.knotvector_u, "-vKnot", *surf.knotvector_v, "-controlPts", *controlPts.flatten())
 
     # Update patchTag, nodeStartTag and materialTag
     lastElTag = ops.getEleTags()[-1]
@@ -183,16 +184,24 @@ for i in range(len(container)):
 
 
 # Creating constraints
-for n in np.arange(1, 17):
+for n in ops.getNodeTags():
     n = int(n)
     if n in [1, 2, 3, 4]:
         ops.fix(n, 1, 1, 1)
-    else:
-        ops.fix(n, 1, 1, 0)
+    # else:
+    #     ops.fix(n, 1, 1, 0)
 
 # Creating equal dof's in patch interface
-for n in [9, 10]:
+for n in [7, 8]:
     ops.equalDOF(n, n + 2, 1, 2, 3)
+
+nodesOnPatches = [5, 6, 7, 8, 11, 12]
+nodesOnBendingStrip = [17, 20, 18, 21, 19, 22]
+
+for i in range(len(nodesOnPatches)):
+    const = int(nodesOnPatches[i])
+    ret = int(nodesOnBendingStrip[i])
+    ops.equalDOF(const, ret, 1, 2, 3)
 
 
 print("ops.getEleTags(): ", ops.getEleTags())
@@ -216,8 +225,8 @@ print("Loading nodes")
 Pz = 1e2
 # for n in [7, 8]:
 for n in [15, 16]:
-    # ops.load(n,0,0,-Pz/2.0)
-    ops.load(n, 0, Pz / 2.0, 0)
+    ops.load(n, 0, 0, -Pz / 2.0)
+    # ops.load(n, 0, Pz / 2.0, 0)
 print("Finished loading nodes")
 
 
@@ -256,50 +265,77 @@ ops.analyze(1)
 print("Finished analysis!")
 
 
-# Haven't coded deformation for multipatch yet
-exit()
-
-controlPts = surf.ctrlpts2d[:]
-controlPts = compatibility.flip_ctrlpts2d(controlPts)
-
-fDef = 1e2
-i = 1
-for dim in controlPts:
-    for point in dim:
-        point[:3] += fDef * np.array(ops.nodeDisp(i))
-        i += 1
-
-controlPts = compatibility.flip_ctrlpts2d(controlPts)
-
-controlPts = (np.array(controlPts).reshape(
-    surf.ctrlpts_size_u * surf.ctrlpts_size_v, 4)).tolist()
-
-# Setting control points for surface
-surf.set_ctrlpts(controlPts, 2, 4)
-
-
-# Set knot vectors
-uKnot = generateKnotVector(surf.degree_u, surf.ctrlpts_size_u)
-vKnot = generateKnotVector(surf.degree_v, surf.ctrlpts_size_v)
-
-surf.knotvector_u = uKnot
-surf.knotvector_v = vKnot
-
-noPtsX = surf.ctrlpts_size_u
-noPtsY = surf.ctrlpts_size_v
-
-# Visualize surface
-surfVisualize(surf, hold=True)
-
-print("ops.nodeDisp(7,2): ", 1000 * np.array(ops.nodeDisp(7)), "mm")
-print("ops.nodeDisp(8,2): ", 1000 * np.array(ops.nodeDisp(8)), "mm")
-
 I = (Lb * (sum(thickness)**3)) / 12.0
-elasticSolution = (Pz * (La**3)) / (3 * E1 * I)
+elasticSolution = (Pz * ((2 * La)**3)) / (3 * E1 * I)
 
-result = 1000 * np.array(ops.nodeDisp(7, 3))
+result = np.array(ops.nodeDisp(16, 3))
+# result = np.array(ops.nodeDisp(8, 3))
 
 print("result: ", result)
-print("elasticSolution: ", 1000 * elasticSolution)
+print("elasticSolution: ", elasticSolution)
 
-print("Done")
+nodesOnLeftPatch = [1, 2, 3, 4, 5, 6, 7, 8]
+nodesOnRightPatch = [9, 10, 11, 12, 13, 14, 15, 16]
+nodesOnBendingStrip = [17, 18, 19, 20, 21, 22]
+
+controlPts_leftPatch = np.array(compatibility.flip_ctrlpts2d(surfL.ctrlpts2d[:])).reshape([surfL.ctrlpts_size_v * surfL.ctrlpts_size_u, 4]).tolist()
+controlPts_rightPatch = np.array(compatibility.flip_ctrlpts2d(surfR.ctrlpts2d[:])).reshape([surfR.ctrlpts_size_v * surfR.ctrlpts_size_u, 4]).tolist()
+controlPts_bendingStrip = np.array(compatibility.flip_ctrlpts2d(bendingStrip.ctrlpts2d[:])).reshape([bendingStrip.ctrlpts_size_v * bendingStrip.ctrlpts_size_u, 4]).tolist()
+
+fdef = 1e2
+for n in ops.getNodeTags():
+    # Adding deformation to controlPts
+    if n in nodesOnLeftPatch:
+        indexN = nodesOnLeftPatch.index(n)
+        point = controlPts_leftPatch[indexN]
+
+    elif n in nodesOnRightPatch:
+        indexN = nodesOnRightPatch.index(n)
+        point = controlPts_rightPatch[indexN]
+
+    elif n in nodesOnBendingStrip:
+        indexN = nodesOnBendingStrip.index(n)
+        point = controlPts_bendingStrip[indexN]
+
+    point[0] = ops.nodeCoord(n)[0] + fdef * ops.nodeDisp(n)[0]
+    point[1] = ops.nodeCoord(n)[1] + fdef * ops.nodeDisp(n)[1]
+    point[2] = ops.nodeCoord(n)[2] + fdef * ops.nodeDisp(n)[2]
+
+
+# # Setting control points for surfaces
+# controlPts_leftPatch = compatibility.flip_ctrlpts2d(controlPts_leftPatch, surfL.ctrlpts_size_u, surf.ctrlpts_size_v)
+# controlPts_rightPatch = compatibility.flip_ctrlpts2d(controlPts_rightPatch, surfR.ctrlpts_size_u, surfR.ctrlpts_size_v)
+# controlPts_bendingStrip = compatibility.flip_ctrlpts2d(controlPts_bendingStrip, bendingStrip.ctrlpts_size_u, bendingStrip.ctrlpts_size_v)
+
+
+
+
+surfL.degree_u=3
+surfL.degree_v=1
+surfL.set_ctrlpts(controlPts_leftPatch, 4, 2)
+surfL.knotvector_u=knotvector.generate(surfL.degree_u, surfL.ctrlpts_size_u)
+surfL.knotvector_v=knotvector.generate(surfL.degree_v, surfL.ctrlpts_size_v)
+
+surfR.degree_u=3
+surfR.degree_v=1
+surfR.set_ctrlpts(controlPts_rightPatch, 4, 2)
+surfR.knotvector_u=knotvector.generate(surfR.degree_u, surfR.ctrlpts_size_u)
+surfR.knotvector_v=knotvector.generate(surfR.degree_v, surfR.ctrlpts_size_v)
+
+bendingStrip.degree_u=1
+bendingStrip.degree_v=2
+bendingStrip.set_ctrlpts(controlPts_bendingStrip, 2, 3)
+bendingStrip.knotvector_u=knotvector.generate(bendingStrip.degree_u, bendingStrip.ctrlpts_size_u)
+bendingStrip.knotvector_v=knotvector.generate(bendingStrip.degree_v, bendingStrip.ctrlpts_size_v)
+
+container.sample_size = 5
+for surf in container:
+    surf.evaluate()
+# Visualization configuration
+container.vis = VisMPL.VisSurface(ctrlpts=True, legend=False)
+# container.vis.ctrlpts_offset=0.1
+
+# Render the surface
+evalcolor = ["green", "green", 'blue']
+container.render(evalcolor=evalcolor)
+
